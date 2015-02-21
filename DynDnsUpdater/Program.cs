d46201760cbddf34a2e48a422e3dc446d2df3ff9
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-02-21 12:08:54 +0100

    The code

---
+++ DynDnsUpdater/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace DynDnsUpdater
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceUrl = args[0];
+            var hostName = args[1];
+            var userName = args[2];
+            var password = args[3];
+            
+            var webClient = new WebClient();
+            var ip = webClient.DownloadString("http://sidoine.net/cgi-bin/ip.pl");
+            ip = ip.Trim(new[] {'\r', '\n', ' '});
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(userName + ":" + password));
+            webClient.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", credentials);
+            var url = string.Format("http://{0}/nic/update/?hostname={1}&myip={2}&system=dyndns",
+                serviceUrl, HttpUtility.UrlEncode(hostName), HttpUtility.UrlEncode(ip));
+            var result = webClient.DownloadString(url);
+            Console.Write(result);
+        }
+    }
+}
